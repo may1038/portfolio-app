412c617602e5a27ce864d5ef978f37be4af1fdf4
Tyler May <tylermay@Tylers-MacBook-Pro.local>
2017-09-28 14:56:22 -0500

    added .active to my nav links

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -6,10 +6,12 @@ class BaseLayout extends Component {
     return (
       <div>
         <nav>
-          <NavLink to="/">Home</NavLink>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/portfolio">Portfolio</NavLink>
-          <NavLink to="/contacts">Contacts</NavLink>
+          <NavLink to="/contacts">Contact</NavLink>
           <NavLink to="/references">References</NavLink>
         </nav>
         {this.props.children}
