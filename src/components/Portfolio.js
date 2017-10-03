52604fa1dd1071f8ca2d2cc0b0f9c1b29f663dcc
Tyler May <tylermay@Tylers-MBP.attlocal.net>
2017-10-02 22:22:09 -0500

    added portfolio photos with links

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,24 @@ class Portfolio extends Component {
   render() {
     return (
       <div>
-        <h3>Portfolio</h3>
+        <div>
+          <a href="https://may-blog.herokuapp.com/" target="_blank">
+            <img
+              className="imgBlog"
+              src="https://i.imgur.com/7uEz0JS.jpg"
+              title="source: imgur.com"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://may-threads-kids.herokuapp.com/" target="_blank">
+            <img
+              className="imgThreads"
+              src="https://i.imgur.com/6mjaiGf.png"
+              title="source: imgur.com"
+            />
+          </a>
+        </div>
       </div>
     )
   }
