4d73b4c1364c87a2b8582393fc665681f77f50b2
Tyler May <tylermay@Tylers-MBP.attlocal.net>
2017-10-08 15:31:30 -0500

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,70 +19,72 @@ class Portfolio extends Component {
             <div className="aboutFlex">
               <div>
                 <h1 className="homeTitle">About</h1>
-                <p>
-                  Hi! My name is Tyler and I really enjoy building things. This
-                  is my portfolio website, feel free to poke around and if you
-                  would like to reach out to me please visit the contact tab and
-                  fill out the form.{" "}
-                </p>
-              </div>
-            </div>
-            {/*Backgroud*/}
-            {/*Backgroud*/}
-            {/*Backgroud*/}
-            <div className="backgroundContactWrap">
-              <div>
-                <div>
-                  <h3 className="aboutFontColor">Background</h3>
+                <div className="aboutContentWrapper">
                   <p>
-                    I recently completed a 12-week course aimed at full stack
-                    web development at The Iron Yard. I enjoy building
-                    applications and especially like styling the app.
+                    Hi! My name is Tyler and I really enjoy building things.
+                    This is my portfolio website, feel free to poke around and
+                    if you would like to reach out to me please visit the
+                    contact tab and fill out the form.{" "}
                   </p>
                 </div>
               </div>
-              {/*Contact*/}
-              {/*Contact*/}
-              {/*Contact*/}
-              <div>
-                <h3 className="aboutFontColor">Contact</h3>
-                <div className="spanFlex">
-                  <span>tylermay@usa.com</span>
-                  <span>
-                    <a
-                      className="homeLink"
-                      href="https://github.com/may1038"
-                      target="_blank"
-                    >
-                      github.com/may1038
-                    </a>
-                  </span>
-                  <span>
-                    <a
-                      className="homeLink"
-                      href="https://www.linkedin.com/in/tyler-may-bb31606b/"
-                      target="_blank"
-                    >
-                      spannkedin.com/in/tylermay
-                    </a>
-                  </span>
+              {/*Backgroud*/}
+              {/*Backgroud*/}
+              {/*Backgroud*/}
+              <div className="backgroundContactWrap">
+                <div>
+                  <div className="backgroundDiv">
+                    <h3 className="aboutFontColor">Background</h3>
+                    <p>
+                      I recently completed a 12-week course aimed at full stack
+                      web development at The Iron Yard. I enjoy building
+                      applications and especially like styling.
+                    </p>
+                  </div>
+                </div>
+                {/*Contact*/}
+                {/*Contact*/}
+                {/*Contact*/}
+                <div>
+                  <h3 className="aboutFontColor">Contact</h3>
+                  <div className="spanFlex">
+                    <span>tylermay@usa.com</span>
+                    <span>
+                      <a
+                        className="homeLink"
+                        href="https://github.com/may1038"
+                        target="_blank"
+                      >
+                        github.com/may1038
+                      </a>
+                    </span>
+                    <span>
+                      <a
+                        className="homeLink"
+                        href="https://www.linkedin.com/in/tyler-may-bb31606b/"
+                        target="_blank"
+                      >
+                        linkedin.com/in/tylermay
+                      </a>
+                    </span>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            {/*Resume*/}
-            {/*Resume*/}
-            {/*Resume*/}
-            <div className="resumeFlex">
-              <div>
-                <h3 className="aboutFontColor">Resume</h3>
-                <a
-                  className="homeLink"
-                  href="https://www.linkedin.com/in/tyler-may-bb31606b/"
-                  target="_blank"
-                >
-                  View/Link
-                </a>
+              {/*Resume*/}
+              {/*Resume*/}
+              {/*Resume*/}
+              <div className="resumeFlex">
+                <div>
+                  <h3 className="aboutFontColor">Resume</h3>
+                  <a
+                    className="homeLink"
+                    href="https://www.linkedin.com/in/tyler-may-bb31606b/"
+                    target="_blank"
+                  >
+                    View/Link
+                  </a>
+                </div>
               </div>
             </div>
             {/*Skills*/}
@@ -90,39 +92,52 @@ class Portfolio extends Component {
             {/*Skills*/}
             <div>
               <h1 className="homeTitle">Skills</h1>
-              <div className="skillsFlex">
+              <div>
                 <div className="reactJsDiv">
                   <div className="rowzContent">
                     <div className="iconFlex">
                       <img
+                        className="icon"
                         src="https://www.iconfinder.com/icons/1781641/2230823/48/raster?token=1507487571-oULtoDL4qIzx7yyPU7XvcozIUBwuWC89-ypRA3NG78LO/hxJRadiFdwxRvCQ%3D"
                         alt=""
                       />
                     </div>
                     <div>
+                      <span className="skillsTitle">Languages:</span>
+                      <br />
+                      <span>Javascript</span>
+                      <br />
+                      <span>HTML</span>
+                      <br />
+                      <span>CSS</span>
+                      <br />
+                      <span>SQL</span>
+                    </div>
+                  </div>
+                  <div className="rowzContent">
+                    <div className="iconFlex">
+                      <img
+                        className="icon"
+                        src="https://www.iconfinder.com/icons/1781646/2230828/48/raster?token=1507487513-EHJhKE6r1bLbTWq7m8Z2Wa9FgjBTePMA-Xzg/hc78AZPqAbmsAE4yy0a23Iw%3D"
+                        alt=""
+                      />
+                    </div>
+                    <div>
+                      <span className="skillsTitle">Front-end Framworks:</span>
+                      <br />
                       <span>ReactJS</span>
                     </div>
                   </div>
                   <div className="rowzContent">
                     <div className="iconFlex">
                       <img
-                        src="https://www.iconfinder.com/icons/1781646/2230828/48/raster?token=1507487513-EHJhKE6r1bLbTWq7m8Z2Wa9FgjBTePMA-Xzg/hc78AZPqAbmsAE4yy0a23Iw%3D"
+                        className="icon"
+                        src="https://www.iconfinder.com/icons/1781650/2230832/48/raster?token=1507492526-d3qSy6rqTtZocqPsi6QvzB0FuWjdGvLV-BaQcXRvRHM0NtPNd7nT7S5DPSYA%3D"
                         alt=""
                       />
                     </div>
                     <div>
-                      <span>CSS3</span>
-                    </div>
-                  </div>
-                  <div className="rowzContent">
-                    <div className="iconFlex">
-                      <img
-                        src="https://www.iconfinder.com/icons/1781646/2230828/48/raster?token=1507487513-EHJhKE6r1bLbTWq7m8Z2Wa9FgjBTePMA-Xzg/hc78AZPqAbmsAE4yy0a23Iw%3D"
-                        alt=""
-                      />
-                    </div>
-                    <div>
-                      <span>CSS3</span>
+                      <span>API</span>
                     </div>
                   </div>
                 </div>
@@ -130,34 +145,43 @@ class Portfolio extends Component {
                   <div className="rowzContent">
                     <div className="iconFlex">
                       <img
+                        className="icon"
                         src="https://www.iconfinder.com/icons/1781651/2230833/48/raster?token=1507487606-8i7TLwEbU9RgA57AmWc3exloUG7MFEuy-FUZ5SXMq5rTZE95SUTXPuu78mi0%3D"
                         alt=""
                       />
                     </div>
                     <div>
-                      <span>HTML5</span>
+                      <span>Responsive & Mobile Design</span>
                     </div>
                   </div>
                   <div className="rowzContent">
                     <div className="iconFlex">
                       <img
+                        className="icon"
                         src="https://www.iconfinder.com/icons/1781648/2230830/48/raster?token=1507487683-OmfjHquWwKF7uB40w4suY45QOmLhDKG7-H7C7mp2ZHaVzqbcLWvuUF%2B1TCFU%3D"
                         alt=""
                       />
                     </div>
                     <div>
-                      <span>Javascript</span>
+                      <span className="skillsTitle">Version Control:</span>
+                      <br />
+                      <span>Git</span>
                     </div>
                   </div>
                   <div className="rowzContent">
                     <div className="iconFlex">
                       <img
-                        src="https://www.iconfinder.com/icons/1781648/2230830/48/raster?token=1507487683-OmfjHquWwKF7uB40w4suY45QOmLhDKG7-H7C7mp2ZHaVzqbcLWvuUF%2B1TCFU%3D"
+                        className="icon"
+                        src="https://www.iconfinder.com/icons/1781652/2230834/48/raster?token=1507492421-j93KfixY3O7XpmCFrBuFBPY3CaV1LevX-gNoqhzatJrus%2BXqZxAfAjuT7PNw%3D"
                         alt=""
                       />
                     </div>
                     <div>
-                      <span>Javascript</span>
+                      <span className="skillsTitle">Server-side:</span>
+                      <br />
+                      <span>Sequelize</span>
+                      <br />
+                      <span>PostgresSQL</span>
                     </div>
                   </div>
                 </div>
